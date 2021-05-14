01bae6e3d64814c75e623f2fbc78504c90c19b39
yazeed2 <yazeedmalmarwani@gmail.com>
2021-05-14 04:06:42 +0300

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yazeed/Documents/workspace/rosProject/src
+# Build directory: /home/yazeed/Documents/workspace/rosProject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
