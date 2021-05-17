4dc767c83648f43ce7ba7a4fc5e7ddebe7d3ae13
yazeed2 <yazeedmalmarwani@gmail.com>
2021-05-17 11:11:53 +0300

    added drive_bot.cpp and cmake list

---
+++ build/ball_chaser/CMakeFiles/ball_chaser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ball_chaser.dir/src/drive_bot.cpp.o"
+  "ball_chaser"
+  "ball_chaser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ball_chaser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,3 +8,4 @@
 
 # modified environment variables
 export PWD='/home/yazeed/Documents/workspace/rosProject/build'
+export PYTHONPATH="/home/yazeed/Documents/workspace/rosProject/devel/lib/python3/dist-packages:$PYTHONPATH"
